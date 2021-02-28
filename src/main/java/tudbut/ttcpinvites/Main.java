c730eed7b20e5ef27b2a7ebc20e52d3d2d395133
tudbut <7401dbfa221176f0601fc90c1ada24467e4193d2>
2021-02-28 19:26:46 +0100

    removing points

--- src/main/java/tudbut/ttcpinvites/Main.java
+++ src/main/java/tudbut/ttcpinvites/Main.java
@@ -13,19 +13,42 @@ public class Main {
         Map<String, String> arguments = ArgumentParser.parseDefault(args);
         
         String n = arguments.get("n");
-        String i = arguments.get("i");
-    
+        String d = arguments.get("d");
         TCN tcn = TCN.read(new FileRW("data.tcn").getContent().join("\n"));
-        TCN iTCN = tcn.getSub(i);
-        TCN nTCN = TCN.getEmpty();
-        nTCN.set("inviter", i);
-        nTCN.set("points", 0);
-        tcn.set(n, nTCN);
+        if(n != null) {
+            String i = arguments.get("i");
     
-        iTCN.set("points", iTCN.getFloat("points") + 1f);
-        for (int j = 2; j <= 4 && iTCN.map.containsKey("inviter"); j*=2) {
-            iTCN = tcn.getSub(iTCN.getString("inviter"));
-            iTCN.set("points", iTCN.getFloat("points") + 1f / j);
+            TCN iTCN = tcn.getSub(i);
+            TCN nTCN = TCN.getEmpty();
+            nTCN.set("inviter", i);
+            nTCN.set("points", 0);
+            tcn.set(n, nTCN);
+    
+            iTCN.set("points", iTCN.getFloat("points") + 1f);
+            for (int j = 2; j <= 4 && iTCN.map.containsKey("inviter"); j *= 2) {
+                iTCN = tcn.getSub(iTCN.getString("inviter"));
+                iTCN.set("points", iTCN.getFloat("points") + 1f / j);
+            }
+        }
+        if(d != null) {
+            String i = arguments.get("i");
+    
+            TCN iTCN;
+            if(!d.equals("true")) {
+                TCN dTCN;
+                dTCN = tcn.getSub(d);
+                iTCN = tcn.getSub(dTCN.getString("inviter"));
+            }
+            else {
+                iTCN = tcn.getSub(i);
+            }
+            tcn.set(d, null);
+        
+            iTCN.set("points", iTCN.getFloat("points") - 1f);
+            for (int j = 2; j <= 4 && iTCN.map.containsKey("inviter"); j *= 2) {
+                iTCN = tcn.getSub(iTCN.getString("inviter"));
+                iTCN.set("points", iTCN.getFloat("points") - 1f / j);
+            }
         }
     
         new FileRW("data.tcn").setContent(tcn.toString());
